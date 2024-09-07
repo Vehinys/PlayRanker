dc219558bf37a05b391007c69d2dcd078845fb9b
Albert Lecomte <albert.lecomte1989@gmail.com>
2024-09-07 21:39:31 +0200

    ADD

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -28,6 +28,10 @@ class RegistrationController extends AbstractController
             // encode the plain password
             $user->setPassword($userPasswordHasher->hashPassword($user, $plainPassword));
 
+            if (empty($user->getRoles())) {
+                $user->setRoles(['ROLE_USER']);
+            }
+
             $entityManager->persist($user);
             $entityManager->flush();
 
