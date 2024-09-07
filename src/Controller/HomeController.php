53077fb11f0daabf52572dca7d4fd00fe0755106
Albert Lecomte <albert.lecomte1989@gmail.com>
2024-09-07 16:47:02 +0200

    ADD USER + REGISTER + LOGIN

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,10 +9,16 @@ use Symfony\Component\Routing\Attribute\Route;
 class HomeController extends AbstractController
 {
     #[Route('/accueil', name: 'home')]
-    public function index(): Response
+    public function home(): Response
     {
         return $this->render('pages/home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
+    }
+
+    #[Route('/', name: 'index')]
+    public function index(): Response
+    {
+        return $this->redirectToRoute('home');
     }
 }
