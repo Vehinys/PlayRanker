7d12ff045f3ed01a189cec9c60e088caf813b71c
Albert Lecomte <albert.lecomte1989@gmail.com>
2024-09-10 16:03:35 +0200

    ADD FORGOT PASSWORD

--- src/Controller/ForgotPasswordController.php
+++ src/Controller/ForgotPasswordController.php
@@ -9,8 +9,9 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Csrf\TokenGenerator\TokenGeneratorInterface;
-use Doctrine\ORM\EntityManagerInterface; // Utilisation de EntityManagerInterface
-use Symfony\Component\Routing\Annotation\Route; // Utilisation de l'annotation pour la route
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Service\SendEmail; // Assurez-vous que SendEmail est correctement importé
 
 class ForgotPasswordController extends AbstractController
 {
@@ -28,8 +29,7 @@ class ForgotPasswordController extends AbstractController
         $this->userRepository = $userRepository;
     }
 
-    #[Route('/forgot/password', name: 'forgot_password', method:['POST','GET'] )]
-
+    #[Route('/forgot/password', name: 'forgot_password', methods: ['POST', 'GET'])]
     public function sendRecoveryLink(
         Request $request,
         SendEmail $sendEmail,
@@ -37,12 +37,44 @@ class ForgotPasswordController extends AbstractController
     ): Response
     {
         $form = $this->createForm(ForgotPasswordType::class);
-        $form-> handleRequest($request);
+        $form->handleRequest($request);
 
+        if ($form->isSubmitted() && $form->isValid()) {
+            $email = $form->get('email')->getData(); // Correction ici
 
+            $user = $this->userRepository->findOneBy([
+                'email' => $email
+            ]);
+
+            if (!$user) {
+
+                // Message de succès à afficher à l'utilisateur
+                $this->addFlash('success', 'Un lien de réinitialisation a été envoyé à votre adresse e-mail.');
+                return $this->redirectToRoute('login');
+            } 
+
+            $user->setForgotPasswordToken($tokenGenerator->generateToken())
+                 ->setForgotPasswordTokenResquestedAt(new \DateTimeImmutable('now'))
+                 ->setForgotPasswordMustBeVerifiedBefore(new \DateTimeImmutable('+15 Minutes'));
+
+            $this->entityManager->flush();
+
+            $sendEmail->send([
+                'recipent_email' => $user->getEmail(),
+                'subject'        => 'Modification de vore mot de passe', 
+                'html_template'  => 'pages/forgot_password/index.html.twig',
+                'context'        => [
+                    'user' => $user
+                ]
+            ]);
+
+            $this->addFlash('success', 'Un lien de réinitialisation a été envoyé à votre adresse e-mail.');
+            return $this->redirectToRoute('login');
+        }
 
         return $this->render('pages/forgot_password/index.html.twig', [
             'controller_name' => 'ForgotPasswordController',
+            'form' => $form->createView(), // Passer le formulaire à la vue
         ]);
     }
 }
