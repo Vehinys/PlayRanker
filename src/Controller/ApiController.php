1412589f15a9910880ca6e721b7ded846c27a369
Lecomte Albert <albert.lecomte1989@gmail.com>
2024-09-11 14:06:16 +0200

    ADD Reach

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -3,29 +3,31 @@
 namespace App\Controller;
 
 use App\HttpClient\ApiHttpClient;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ApiController extends AbstractController
 {
-    #[Route('/api', name: 'app_api')]
+    #[Route('/jeux', name: 'jeux')]
     public function index( ApiHttpClient $apiHttpClient): Response
     {
         $games = $apiHttpClient->games();
 
-        return $this->render('api/index.html.twig', [
+        return $this->render('pages/jeux/index.html.twig', [
             'games' => $games
         ]);
     }
 
-    #[Route('/api/search/{input}', name: 'search')]
-    public function search( ApiHttpClient $apiHttpClient, $input): Response
+    #[Route('/jeux/search', name: 'search', methods:'POST')]
+    public function search( ApiHttpClient $apiHttpClient, Request $request): Response
     {
+ 
+        $input = $request->get('input');
         $games = $apiHttpClient->gamesSearch($input);
 
-
-        return $this->render('api/index.html.twig', [
+        return $this->render('pages/jeux/index.html.twig', [
             'games' => $games
         ]);
     }
