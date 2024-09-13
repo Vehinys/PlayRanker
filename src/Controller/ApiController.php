adead3706ced1c1e351259764873243e8e1a066c
Lecomte Albert <albert.lecomte1989@gmail.com>
2024-09-13 12:06:02 +0200

    ADD API

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\HttpClient\ApiHttpClient;
+use Doctrine\ORM\Mapping\Id;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,29 +12,25 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class ApiController extends AbstractController
 {
     #[Route('/jeux', name: 'jeux')]
-    public function index(ApiHttpClient $apiHttpClient, Request $request): Response
+    public function index(ApiHttpClient $apiHttpClient, Request $request, string $id = null): Response
     {
-        // Récupérer le numéro de page depuis la requête (par défaut page 1)
         $page = $request->query->getInt('page', 1);
-        
-        // Appeler la méthode nextPage pour récupérer les jeux paginés
+        // dd($page);
         $games = $apiHttpClient->nextPage($page);
-        
         // dd($games);
 
         return $this->render('pages/jeux/index.html.twig', [
             'games' => $games,
-            'currentPage' => $page,
+            'currentPage' => $page
         ]);
     }
-    
-    
+
     #[Route('/jeux/search', name: 'search', methods: ['POST'])]
     public function search(ApiHttpClient $apiHttpClient, Request $request): Response
     {
         $input = $request->get('input');
-        $games = $apiHttpClient->gamesSearch($input);
         // dd($input);
+        $games = $apiHttpClient->gamesSearch($input);
         // dd($games);
         
         return $this->render('pages/jeux/index.html.twig', [
@@ -46,9 +43,8 @@ class ApiController extends AbstractController
     {
        
         $gameDetail = $apiHttpClient->gameDetail($id);
-        $gameAnnonce = $apiHttpClient->gameAnnonce($id);
-
         // dd($gameDetail);
+        $gameAnnonce = $apiHttpClient->gameAnnonce($id);
         // dd($gameAnnonce);
 
         return $this->render('pages/jeux/detail.html.twig', [
@@ -62,6 +58,7 @@ class ApiController extends AbstractController
     {
         // Utiliser la méthode nextPage de ApiHttpClient pour récupérer les jeux à la page donnée
         $games = $apiHttpClient->nextPage($page);
+        // dd($page);
 
         return $this->render('pages/jeux/index.html.twig', [
             'games' => $games,
