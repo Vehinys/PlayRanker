50ac9f4f87c5503721843e21f3d3af27fca57b3a
Lecomte Albert <albert.lecomte1989@gmail.com>
2024-09-13 17:07:34 +0200

    ADD PROFIL

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -2,17 +2,22 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ProfilController extends AbstractController
 {
     #[Route('/profil', name: 'profil')]
-    public function index(): Response
-    {
+    public function index(
+
+    
+    ): Response {
+
+        $user = $this->getUser();
+    
         return $this->render('pages/profil/index.html.twig', [
-            'controller_name' => 'ProfilController',
+            'user' => $user
         ]);
     }
 }
