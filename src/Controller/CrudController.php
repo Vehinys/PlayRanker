ed72d8ba3c236c59dbf0993906c3fe453146d418
Albert Lecomte <albert.lecomte1989@gmail.com>
2024-10-02 08:58:09 +0200

    Add commentaire

--- src/Controller/CrudController.php
+++ src/Controller/CrudController.php
@@ -26,7 +26,7 @@ class CrudController extends AbstractController
     #[Route('/forum/category/{categoryId}/topic/new', name: 'topic_new')]
     public function newTopic(
 
-        string $categoryId, 
+        int $categoryId, 
         Request $request,
         EntityManagerInterface $manager
 
