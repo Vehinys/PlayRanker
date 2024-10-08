ad2fba027499ed150a9b0371592db8c1b9b7b452
Lecomte Albert <albert.lecomte1989@gmail.com>
2024-10-08 09:05:36 +0200

    EDITE CSS

--- src/Entity/GamesList.php
+++ src/Entity/GamesList.php
@@ -3,8 +3,6 @@
 namespace App\Entity;
 
 use App\Repository\GamesListRepository;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: GamesListRepository::class)]
