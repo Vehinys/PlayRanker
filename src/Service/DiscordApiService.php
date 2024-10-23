94f01c9210c375e0bfe2b03c8dc78b9168279f29
Vehinys <albert.lecomte1989@gmail.com>
2024-10-23 17:01:25 +0200

    add

--- src/Service/DiscordApiService.php
+++ src/Service/DiscordApiService.php
@@ -4,33 +4,34 @@ namespace App\Service;
 
 use App\Model\DiscordUser;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class DiscordApiService
 {
-    const AUTHORIZATION_BASE_URI = 'https://discord.com/api/oauth2/authorize';
-
-    const USERS_ME_ENDPOINT = '/api/users/@me';
+    const AUTHORIZATION_URI = 'https://discord.com/api/oauth2/authorize';
+    const API_VERSION = '10';
+    const USERS_ME_ENDPOINT = 'https://discord.com/api/v' . self::API_VERSION . '/users/@me';
 
     public function __construct(
         private readonly HttpClientInterface $discordApiClient,
         private readonly SerializerInterface $serializer,
-        private readonly string              $clientId,
-        private readonly string              $redirectUri
-    )
-    {
+        private readonly string $clientId,
+        private readonly string $redirectUri,
+    ) {
     }
 
     public function getAuthorizationUrl(array $scope): string
     {
-        return self::AUTHORIZATION_BASE_URI . "?" . http_build_query([
-                'client_id' => $this->clientId,
-                'redirect_uri' => $this->redirectUri,
-                'response_type' => 'token',
-                'scope' => implode(' ', $scope),
-                'prompt' => 'none'
-            ]);
+        $queryParameters = http_build_query([
+            'client_id'     => $this->clientId,
+            'redirect_uri'  => $this->redirectUri,
+            'response_type' => 'token',
+            'scope'         => implode(' ', $scope),
+            'prompt'        => 'none',
+        ]);
+
+        return self::AUTHORIZATION_URI . '?' . $queryParameters;
     }
 
     public function fetchUser(string $accessToken): DiscordUser
